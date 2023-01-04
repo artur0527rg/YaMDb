7ae98a9a24dcd237a0ba575ef6b66de0264f29bc
ARTUR <rogozinskiyartur@gmail,com>
2023-01-04 15:07:42 +0300

    regisetred Genre, Category, Title in admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Genre, Category, Titile
+
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Titile)
