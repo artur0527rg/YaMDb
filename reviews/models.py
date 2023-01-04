e0ff15d87111b6d847c6eafea316433634088ffd
ARTUR <rogozinskiyartur@gmail,com>
2023-01-04 15:01:43 +0300

    Category, Genre, Title models created

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
+def year_validator(value):
+    """Валидатор для поля year, модели Title."""
+
+    if value > timezone.now().year:
+        raise ValidationError(
+            'Год выпуска не может быть больше настоящего',
+            params={'value': value},
+        )
 
 USER = 'user'
 MODERATOR = 'moderator'
@@ -26,3 +35,47 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ['username']
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=255
+    )
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return self.slug
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=255
+        )
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self) -> str:
+        return self.slug
+
+class Titile(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    year = models.IntegerField(validators=[year_validator])
+    description = models.TextField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(
+        Category,
+        related_name='titles',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    class Meta:
+        ordering = ['year']
+
+    def __str__(self) -> str:
+        return self.name
